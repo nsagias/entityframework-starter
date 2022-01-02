764aba13991d341a8bb8f6fecb40214ecdf4881a
Nick <nsagias@gmail.com>
2022-01-02 17:22:52 -0500

    Create console appskeleton

---
+++ EntityFrameWork/EntityFrameworkNet.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EntityFrameworkNet.ConsoleApp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
