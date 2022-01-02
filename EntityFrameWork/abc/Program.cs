844b0123aa23c0c128f44faf6ab080969a3b45c2
Nick <nsagias@gmail.com>
2022-01-02 17:39:41 -0500

    Remove console app as it is wrong version

---
+++ EntityFrameWork/abc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace abc
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
