a1303f09f0ecf98b245ed9dc84b0421e7057cbd1
Nick <nsagias@gmail.com>
2022-01-02 18:16:11 -0500

    Rebuild using entity framework 3.1, after re-installing

---
+++ EntityFrameworkNet/EntityFrameworkNet.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EntityFrameworkNet.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
