434ce0d3d92362f7977f0204dd4541ddd284ce38
Nick <nsagias@gmail.com>
2022-01-02 20:50:24 -0500

    Setup connection for localdb on MSSQLLocalDB

--- EntityFrameworkNet/EntityFrameworkNet.Data/FootballLeagueDbContext.cs
+++ EntityFrameworkNet/EntityFrameworkNet.Data/FootballLeagueDbContext.cs
@@ -10,6 +10,11 @@ namespace EntityFrameworkNet.Data
         {
         }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB; Intitial Catalog=FootballLeague_EfCore");
+        }
         // Link to tables
         public DbSet<Team> Teams { get; set; }
         public DbSet<League> Leagues { get; set; }
