97ad002eb021315a9d1cb60712aae11b787362f1
Nick <nsagias@gmail.com>
2022-01-02 19:40:13 -0500

    Create class/model League

---
+++ EntityFrameworkNet/EntityFrameworkNet.Domain/League.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace EntityFrameworkNet.Domain
+{
+    public class League
+    {
+        public League()
+        {       
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
