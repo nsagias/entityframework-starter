eeea0b9e732c4310f9310d0c9acdf19da7aa8933
Nick <nsagias@gmail.com>
2022-01-02 19:45:33 -0500

    Create Team model and with foreign key connected to League

---
+++ EntityFrameworkNet/EntityFrameworkNet.Domain/Team.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace EntityFrameworkNet.Domain
+{
+    public class Team
+    {
+        public Team()
+        {
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        // foreign key
+        public int LeagueId { get; set; }
+        public virtual League League { get; set; }
+     }
+}
