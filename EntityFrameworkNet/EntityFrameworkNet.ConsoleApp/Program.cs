cfe3ad3e7c1a794b812662088b9006ac21ce0eec
Nick <nsagias@gmail.com>
2022-01-03 04:07:42 -0500

    Add new league and context

--- EntityFrameworkNet/EntityFrameworkNet.ConsoleApp/Program.cs
+++ EntityFrameworkNet/EntityFrameworkNet.ConsoleApp/Program.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using EntityFrameworkNet.Data;
+using EntityFrameworkNet.Domain;
+using System;
 
 namespace EntityFrameworkNet.ConsoleApp
 {
     class Program
     {
+        private static FootballLeagueDbContext context = new FootballLeagueDbContext();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            context.Leagues.Add(new League { Name = "League with Toronto FC Division" });
+            context.SaveChangesAsync();
+
+
+            Console.WriteLine("Press any key to end...");
+            Console.ReadKey();
         }
     }
 }
